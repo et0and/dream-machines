9328ca3736430cbf7b17f771ee7f4496c63ea3e3
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2022-09-29 10:30:04 +1300

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ var TEMPLATE = dedent`
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/p5.min.js"></script>
+    <script src="sketch.js"></script>
 
     %%HEAD%%
   </head>
